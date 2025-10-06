655b72339178e3655556e91ace0d294884887bf8
charliel29 <charliel29@nycstudents.net>
2025-10-06 09:25:06 -0400

    french english

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
-def language(x):
+""" def language(x):
     print ("do something")
+language("Cadee's computer never seems to work in the morning") """
 
-language("Cadee's computer never seems to work in the morning")
+
+
+def lang():
+   s = 0
+   t = 0
+   x = input("sentence")
+   for char in x:
+     if char == "s" or "S":
+       s=s+1
+     elif char == "t" or "T"
+        t=+1
+    if s >= t:
+    print ("french")
+    else:
+ print ("english")
+lang()
+
